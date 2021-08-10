0a3b702e79a0a23dc7fe2c59c2f5f3511dcfc3a9
Scott Balun <sbalun@gmail.com>
2021-08-10 14:26:21 -0400

    exercises related to anonymous and arrow functions

--- 04-functions.js
+++ 04-functions.js
@@ -2,4 +2,18 @@ function sum(a, b){
     return a+b;
 }
 
-console.log("the total value is " + sum(2,3));
+function product(a,b){
+    return a*b;
+}
+
+console.log("the sum is " + sum(2,3));
+console.log("the product is " + product(2,3));
+
+// Using an anonymous function as the callback for setTimeout
+setTimeout( function(){
+    console.log("I'm coming from the anonymous function!")
+},1000);
+
+// Using an arrow function as the callback for setTimeout
+// Works just like an anonymous function but less code
+setTimeout(()=> console.log("I'm coming from the arrow function!"),1000);
